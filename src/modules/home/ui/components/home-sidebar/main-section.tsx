acf30f5841ed63fdbb18e95fcb4f63ddddf36a4e
ivanmeda993 <ivanmilicevicmeda@gmail.com>
2025-04-10 20:50:42 +0200

    Refactor home page and add new UI components.
    Removed the old home page implementation and introduced structured, reusable modular components for the home navbar, sidebar, and search input. Added several new dependencies like Radix UI and Zod to support UI functionality and form management.

---
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "Subscriptions",
+    url: "/feed/subscriptions",
+    icon: PlaySquareIcon,
+  },
+  {
+    title: "Trending",
+    url: "/feed/trending",
+    icon: FlameIcon,
+  },
+];
+export const MainSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.url}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false}
+                onClick={() => {}}
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
