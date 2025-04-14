8ccdb2f22bee4da0979009a67cc820de46f4c8be
ivanmeda993 <ivanmilicevicmeda@gmail.com>
2025-04-14 18:33:40 +0200

    Update route matcher to protect '/studio' and its subpaths
    Revised the `isProtected` matcher to include '/studio' and all its subroutes. This ensures the middleware now secures access to the entire '/studio' path structure.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtected = createRouteMatcher(["/protected"]);
+const isProtected = createRouteMatcher(["/studio(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtected(req)) {
